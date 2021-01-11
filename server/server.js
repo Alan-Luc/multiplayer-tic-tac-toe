c49c5258b24fca47fda5d5284997a2e5160d91d1
alan-pc <alanluc9@gmail.com>
2021-01-11 14:27:43 -0500

    latest version

--- server/server.js
+++ server/server.js
@@ -1,65 +1,67 @@
-const http = require('http');
-const express = require('express');
+const http = require("http");
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
-const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 
-const router = require('./router');
+const router = require("./router");
 //App Setup
 const server = http.createServer(app);
 
 //Socket Setup
-const io = require('socket.io')(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"],
-        allowedHeaders: ["Access-Control-Allow-Origin"],
-        credentials: true,
-    }
-})
-
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Access-Control-Allow-Origin"],
+    credentials: true,
+  },
+});
 
 app.use(cors());
 app.use(router);
 
-io.on('connect', (socket) => {
-    console.log('made socket connection', socket.id)
-    socket.on('join', ({ name, room }, callback) => {
-        const { error, user } = addUser({ id: socket.id, name, room });
+io.on("connect", (socket) => {
+  console.log("made socket connection", socket.id);
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
 
-        if(error) return callback(error);
+    if (error) return callback(error);
 
-        socket.join(user.room);
+    socket.join(room);
 
-        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!`});
+    io.to(room).emit('roomData', { room: room, users: getUsersInRoom(room) });
 
-        //io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+    callback();
+  });
 
-        callback();
-    })
+  socket.on("move", ({ name, room, move, location, turn }) => {
+    //const user = getUser(socket.id);
+    console.log(move);
+    io.to(room).emit("move", { move, location, turn, name });
+  });
 
-    socket.on('move', ({ name, room, move}) => {
-        const user = getUser(socket.id);
-        console.log(move);
-        io.to(room).emit('move', { user: name, move })
-    })
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
 
-    socket.on('disconnect', () => {
-        const user = removeUser(socket.id);
-    
-        if(user) {
-          io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
-          //io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
-        }
-    })
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "Admin",
+        text: `${user.name} has left.`,
+      });
+      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+    }
+  });
 
-    /*if(io.sockets.clients('room').length === 2) { // max two clients
+  /*if(io.sockets.clients('room').length === 2) { // max two clients
     socket.emit('full', room);
     }*/
-    
-})
+});
+
+server.listen(process.env.PORT || 8000, () =>
+  console.log("Server has started.")
+);
 
 
-
-server.listen(process.env.PORT || 8000, () => console.log('Server has started.'));
+//socket
--- src/client/components/Game.js
+++ src/client/components/Game.js
@@ -1,150 +1,192 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import queryString from 'query-string'
-import io from 'socket.io-client'
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import queryString from "query-string";
+import io from "socket.io-client";
 
-import Board from '../components/Board'
-import './Game.css'
-import { query } from 'express'
+import Board from "../components/Board";
+import "./Game.css";
 
-let socket = io('http://localhost:8000', {
-    withCredentials: true
+let socket = io("http://localhost:8000", {
+  withCredentials: true,
 });
 //const socket = io.connect('http://localhost:3000');
 
 const Game = ({ location }) => {
-    const [playerXTurn, setPlayerXTurn] = useState(true);
-    const [squares, setSquares] = useState(Array(9).fill(null));
-    const [stepNumber, setStepNumber] = useState(0);
-    const [status, setStatus] = useState("It is X's turn");
-    //const [name, setName] = useState('');
-    //const [room, setRoom] = useState('');
-    const [users, setUsers] = useState('');
-    const [flag, setFlag] = useState(0);
-    const [moves, setMoves] = useState(undefined);
-    const ENDPOINT = 'http://localhost:8000';
+  const [playerXTurn, setPlayerXTurn] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [stepNumber, setStepNumber] = useState(0);
+  const [status, setStatus] = useState("It is X's turn");
+  const [name, setName] = useState([]);
+  const [room, setRoom] = useState("");
+  const [users, setUsers] = useState('');
+  //const users = useRef();
+  const [flag, setFlag] = useState(0);
+  const [moves, setMoves] = useState(undefined);
+  const ENDPOINT = "http://localhost:8000";
+  //const users2 = users.slice();
 
-    useEffect(() => {
-        //const { name, room } = queryString.parse(location.search);
+  useEffect(() => {
+    //const { name, room } = queryString.parse(location.search);
 
-        socket = io.connect(ENDPOINT);
+    socket = io.connect(ENDPOINT);
+    const square = squares.slice();
+    
+    setName(queryString.parse(location.search).name);
 
-        //setRoom(room);
-        //setName(name);
+    socket.emit(
+      "join",
+      {
+        name: queryString.parse(location.search).name,
+        room: queryString.parse(location.search).room,
+      },
+      (error) => {
+        if (error) {
+          setFlag(1);
+          alert(error);
+        }
+      }
+    );
+    /*socket.on("move", (data) => {
+      console.log(data.move);
+    });*/
+    setRoom(room);
 
-        socket.emit('join', {
-            name: queryString.parse(location.search).name, 
-            room: queryString.parse(location.search).room 
-        }, 
-        (error) => {
-            if(error) {
-                setFlag(1);
-                alert(error);
-            }
-        });
-        socket.on('move', (data) => {
-            console.log(data.move)
-        })
-        
-    }, [ENDPOINT, location.search]);
+    socket.on('move', ({ move, location, turn, name }) => {
+        square[location] = move;
+        setSquares(move);
+        setPlayerXTurn(turn);
+        turn ? setStatus(`It is ${name}'s turn`) : setStatus(`It is ${name}'s turn`);
+        console.log(squares);
+        console.log(move);
+        console.log(turn);
+        console.log(name);
+        console.log(users.length);
+    });
+
+    socket.on('roomData', ({ users }) => {
+        setUsers(users);
+        console.log(users);
+    })
+  }, [ENDPOINT, location.search]);
 
     /*useEffect(() => {
         socket.on('roomData', ({ users }) => {
             setUsers(users);
+            console.log(users);
         })
     }, []);*/
 
-    const handleClick = (i) => {
-        const square = squares.slice()
-        const win = winner(square)
-        const tie = stepNumber===8
+  const handleClick = (i) => {
+    const square = squares.slice();
+    const win = winner(square);
+    const tie = stepNumber === 8;
 
-        if(win || squares[i]) {
-            return;
-        }
-        
-        if(squares[i] === null) {
-            square[i] = playerXTurn ? 'X' : 'O'; 
-            setSquares(square);
-            setPlayerXTurn(!playerXTurn);
-            setStepNumber(stepNumber + 1);
-            !playerXTurn ? setStatus("It is X's turn") : setStatus("It is O's turn");
-        }
-        //squares[i] === null ? (squares[i] = this.state.playerXTurn ? 'X' : 'O';) : this.setState({status: 'Please select another tile'});
-        //squares[i] === 'X' ? this.setState({playerXTurn: !playerXTurn}) : this.setState({playerXTurn: }) 
-        
-        if(win) {
-            setStatus('');
-        }
-        else if(tie)  {
-            setStatus('Tie');
-        }
-
-        socket.emit('move', {
-            name: queryString.parse(location.search).name,
-            room: queryString.parse(location.search).room,
-            move: squares
-        });
+    if (win || squares[i]) {
+      return;
     }
 
-    const handleReset = () => {
-        setPlayerXTurn(true);
-        setSquares(Array(9).fill(null));
-        setStepNumber(0);
-        setStatus("It is X's turn");
+    if (squares[i] === null) {
+      square[i] = playerXTurn ? "X" : "O";
+      setSquares(square);
+      setPlayerXTurn(!playerXTurn);
+      setStepNumber(stepNumber + 1);
+      //!playerXTurn ? setStatus("It is X's turn") : setStatus("It is O's turn");
     }
+    //squares[i] === null ? (squares[i] = this.state.playerXTurn ? 'X' : 'O';) : this.setState({status: 'Please select another tile'});
+    //squares[i] === 'X' ? this.setState({playerXTurn: !playerXTurn}) : this.setState({playerXTurn: })
+
+    if (win) {
+      setStatus("");
+    } else if (tie) {
+      setStatus("Tie");
+    }
+
+    socket.emit("move",  {
+      name: queryString.parse(location.search).name,
+      room: queryString.parse(location.search).room,
+      move: square,
+      location: i,
+      turn: !playerXTurn
+
+    });
+
     
-    const winner = (squares) => {
-        const win = [
-            [0,1,2],
-            [3,4,5],
-            [6,7,8],
-            [0,3,6],
-            [1,4,7],
-            [2,5,8],
-            [0,4,8],
-            [2,4,6]
-        ]
-    
-        for(let i = 0; i<win.length; i++) {
-            const [a,b,c] = win[i]
-            if(squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
-                return squares[a]
-            }
-        }
-        return null
+  };
+
+  const handleReset = () => {
+    setPlayerXTurn(true);
+    setSquares(Array(9).fill(null));
+    setStepNumber(0);
+    setStatus("It is X's turn");
+  };
+
+  const winner = (squares) => {
+    const win = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < win.length; i++) {
+      const [a, b, c] = win[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[b] === squares[c]
+      ) {
+        return squares[a];
+      }
     }
+    return null;
+  };
 
-    let victory;
-    const win = winner(squares);
-    if(win) {
-        victory = 'The winner is ' + (playerXTurn? 'O' : 'X')
-    }
+  let victory;
+  const win = winner(squares);
+  if (win) {
+    victory = "The winner is " + (playerXTurn ? "O" : "X");
+  }
 
-    return (
-        <div className = 'body'>
-                <div className = 'game'>
-                <div>
-                    <Board
-                    onClick = {(i)=>handleClick(i)}
-                    squares = {squares}
-                    />
-                    <br />
-                    {win? <div id = 'feedback' className = 'status'>{victory}</div> : <div className = 'status'>{status}</div>}
-                    <br />
+  console.log(queryString.parse(location.search).room);
 
-                    <div className = 'win'>
-                        {(win || stepNumber === 9) && <button  className = 'reset' onClick = {()=>handleReset()}>Play Again?</button>}
-                        <br></br><br></br>{(win || stepNumber === 9) && <Link to={'/'}>
-                        <button className = 'button2'>Exit</button>
-                        </Link>}
-                    </div>
-                    <br></br>
-                    
-                </div>
+  return (
+    <div className='body'>
+      <div className='game'>
+        <div>
+          <Board onClick={(i) => handleClick(i)} squares={squares} />
+          <br />
+          {win ? (
+            <div id='feedback' className='status'>
+              {victory}
             </div>
+          ) : (
+            <div className='status'>{status}</div>
+          )}
+          <br />
+
+          <div className='win'>
+            {(win || stepNumber === 9) && (
+              <button className='reset' onClick={() => handleReset()}>
+                Play Again?
+              </button>
+            )}
+            <br></br>
+            <br></br>
+            {(win || stepNumber === 9) && (
+              <Link to={"/"}>
+                <button className='button2'>Exit</button>
+              </Link>
+            )}
+          </div>
+          <br></br>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Game;
